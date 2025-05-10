72f84a9dddd07cf096bd2a3075d29cafbaa59584
Kar Hor Yap <khyap17274@gmail.com>
2025-05-10 13:32:04 +0100

    test connection with azure-ai-ml sdk

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+from az_config import subscription_id, resource_group
+
 def main():
+    
     print("Hello from aml-v2-deployment!")
+
+    ml_client = MLClient(
+        DefaultAzureCredential(), subscription_id, resource_group
+    )
 
 
 if __name__ == "__main__":
