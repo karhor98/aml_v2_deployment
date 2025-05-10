c49ca0efb5a053024edf56826ac1d10b3ee5179f
Kar Hor Yap <khyap17274@gmail.com>
2025-05-10 15:35:21 +0100

    added create compute instance function

--- aml_setup.py
+++ aml_setup.py
@@ -1,8 +1,9 @@
-from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
+from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential, TokenCachePersistenceOptions, SharedTokenCacheCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.ai.ml import MLClient
-from azure.ai.ml.entities import Workspace
+from azure.ai.ml.entities import Workspace, ComputeInstance, AmlCompute
 from az_config import RESOURCE_GROUP, SUBSCRIPTION_ID, AML_WORKSPACE
+import datetime
 
 # Initialize credentials and client
 try:
@@ -10,8 +11,6 @@ try:
     # Check if given credential can get token successfully.
     credential.get_token("https://management.azure.com/.default")
 except Exception as ex:
-    # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
-    # This will open a browser page for
     credential = InteractiveBrowserCredential()
 
 def create_resource_group(resource_client, resource_group_name, location):
@@ -53,26 +52,45 @@ def create_aml_workspace(ml_client, resource_group_name, workspace_name, locatio
 
     try:
         # Check if the workspace already exists
-        existing_workspace = ml_client.workspaces.get(resource_group_name, workspace_name)
+        existing_workspace = ml_client.workspaces.get(workspace_name)
         if existing_workspace:
             print(f"Azure Machine Learning workspace '{workspace_name}' already exists in resource group '{resource_group_name}'.")
             return
+        
     except Exception:
         # If the workspace does not exist, an exception will be raised
         print(f"Azure Machine Learning workspace '{workspace_name}' does not exist. Creating a new one.")
     
-    ## Create the workspace
-    workspace = Workspace(
-        name=workspace_name,
-        location=location,
-        display_name="Op Insight AML Workspace",
-        description="Workspace created via Python SDK",
-        tags={"purpose": "demo"},
-        )
+        ## Create the workspace
+        workspace = Workspace(
+            name=workspace_name,
+            location=location,
+            display_name="Op Insight AML Workspace",
+            description="Workspace created via Python SDK",
+            tags={"purpose": "demo"},
+            )
+        
+        # Begin workspace creation
+        ml_client.workspaces.begin_create(workspace)
+        print(f"Azure Machine Learning workspace '{workspace_name}' created in resource group '{resource_group_name}'.")
+
+def create_compute_instance(ml_client, workspace_name, compute_instance_name):
+    """
+    Create a compute instance in the Azure Machine Learning workspace.
     
-    # Begin workspace creation
-    ml_client.workspaces.begin_create(workspace)
-    print(f"Azure Machine Learning workspace '{workspace_name}' created in resource group '{resource_group_name}'.")
+    This function creates a new compute instance in the specified Azure Machine Learning workspace.
+    
+    """
+    ci_basic_name = compute_instance_name + datetime.datetime.now().strftime("%Y%m%d%H%M")
+    ci_basic = ComputeInstance(name=ci_basic_name, 
+                               size="STANDARD_DS3_v2",
+                               tags={"purpose": "demo"})
+    
+    print(ci_basic)
+    
+    ml_client.begin_create_or_update(ci_basic).result()
+
+    print(f"Compute instance '{ci_basic_name}' created in workspace '{workspace_name}'.")
 
 if __name__ == "__main__":
 
@@ -85,4 +103,5 @@ if __name__ == "__main__":
     # Define workspace parameters
     # Create AML workspace
     create_aml_workspace(ml_client, RESOURCE_GROUP, AML_WORKSPACE, location='uksouth')
-    
+    # Create Compute instance
+    # create_compute_instance(ml_client, AML_WORKSPACE, "basic-ci")
